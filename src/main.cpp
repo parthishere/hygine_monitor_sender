bf6c8adde4af7622ae23de3df0b783f4a84f573a
parthishere <parthishere1234@gmail.com>
2023-04-11 12:12:24 +0530

    working with inturrept

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,11 @@
 
 // set the pins to shutdown
 #define SHT_LOX1 4
+const byte VL53LOX_1_InterruptPin = 10;
+
+
 #define SHT_LOX2 5
+const byte VL53LOX_2_InterruptPin = 6;
 
 // DC:54:75:50:E3:E4: DFBot C3
 // 40:22:D8:4F:03:C8 motu esp
@@ -41,6 +45,10 @@ uint16_t distance = 0;
 bool decreased = false, increased = true;
 String esid;
 bool direction_in{false}, phase_one_for_in{false}, phase_two_for_in{}, phase_one_for_out{}, phase_two_for_out{};
+
+
+volatile byte VL53LOX_1_State = LOW;  
+volatile byte VL53LOX_2_State = LOW;  
 
 typedef struct struct_message
 {
@@ -76,14 +84,13 @@ int32_t getWiFiChannel(const char *ssid)
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-
 }
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
 {
   // Copies the sender mac address to a string
   char macStr[18];
-  
+
   memcpy(&SsidRecieved, incomingData, sizeof(SsidRecieved));
 
   Serial.println("This is ssid");
@@ -91,7 +98,7 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
 
   if (qsid.length() > 0)
   {
-    
+
     for (int i = 0; i < 96; ++i)
     {
       EEPROM.write(i, 0);
@@ -110,6 +117,17 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
   }
 }
 
+
+void VL53LOXISR1() {
+  VL53LOX_1_State = digitalRead(VL53LOX_1_InterruptPin);
+  
+}
+
+void VL53LOXISR2() {
+  VL53LOX_2_State = digitalRead(VL53LOX_2_InterruptPin);
+}
+
+
 void setup()
 {
 
@@ -122,7 +140,6 @@ void setup()
   EEPROM.begin(512);
   delay(10);
 
-  
   for (int i = 0; i < 32; ++i)
   {
     esid += char(EEPROM.read(i));
@@ -165,6 +182,12 @@ void setup()
   pinMode(SHT_LOX1, OUTPUT);
   pinMode(SHT_LOX2, OUTPUT);
 
+  pinMode(VL53LOX_1_InterruptPin, INPUT_PULLUP);
+  pinMode(VL53LOX_2_InterruptPin, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(VL53LOX_1_InterruptPin), VL53LOXISR1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(VL53LOX_2_InterruptPin), VL53LOXISR2, CHANGE);
+
   Serial.println(F("Shutdown pins inited..."));
 
   digitalWrite(SHT_LOX1, LOW);
@@ -182,65 +205,76 @@ long t2 = 0;
 
 void loop()
 {
-  lox1.rangingTest(&measure1, false); 
-  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
-
-
-
-  myData.distance_1_mm = measure1.RangeMilliMeter;
-  myData.distance_2_mm = measure2.RangeMilliMeter;
-  if (myData.distance_1_mm < 2000)
+  if (VL53LOX_1_State == LOW)
   {
-    t1 = millis();
-  }
-  else if (myData.distance_2_mm < 2000)
-  {
-    t2 = millis();
-  }
-  if (t1 > 0 && t2 > 0)
-  {
-    if (t1 < t2)
+    Serial.println("sensor 1 intterupt");
+
+    lox1.getRangingMeasurement(&measure1, false);
+    lox2.getRangingMeasurement(&measure2, false); // pass in 'true' to get debug data printout!
+
+    myData.distance_1_mm = measure1.RangeMilliMeter;
+    myData.distance_2_mm = measure2.RangeMilliMeter;
+    if (myData.distance_1_mm < 2000)
     {
-
-      phase_one_for_in = true;
-      phase_one_for_out = false;
+      t1 = millis();
     }
-    else if (t2 < t1)
+    else if (myData.distance_2_mm < 2000)
     {
+      t2 = millis();
+    }
+    if (t1 > 0 && t2 > 0)
+    {
+      if (t1 < t2)
+      {
 
-      phase_one_for_out = true;
+        phase_one_for_in = true;
+        phase_one_for_out = false;
+      }
+      else if (t2 < t1)
+      {
+
+        phase_one_for_out = true;
+        phase_one_for_in = false;
+      }
+      else
+      {
+        Serial.println("");
+      }
+
+      t1 = 0;
+      t2 = 0;
+    }
+    if (myData.distance_1_mm > myData.distance_2_mm && phase_one_for_in && !phase_one_for_out)
+    {
+      Serial.println("in");
       phase_one_for_in = false;
+      phase_one_for_out = false;
+      myData.direction = 1;
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+      t1 = 0;
+      t2 = 0;
     }
-    else
+    if (myData.distance_1_mm < myData.distance_2_mm && phase_one_for_out && !phase_one_for_in)
     {
-      Serial.println("");
+      Serial.println("out");
+      phase_one_for_out = false;
+      phase_one_for_in = false;
+      phase_one_for_out = false;
+      myData.direction = 0;
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+      t1 = 0;
+      t2 = 0;
     }
+    lox1.clearInterruptMask(false);
 
-    t1 = 0;
-    t2 = 0;
-  }
-  if (myData.distance_1_mm > myData.distance_2_mm && phase_one_for_in && !phase_one_for_out)
-  {
-    Serial.println("in");
-    phase_one_for_in = false;
-    phase_one_for_out = false;
-    myData.direction = 1;
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-    t1 = 0;
-    t2 = 0;
-  }
-  if (myData.distance_1_mm < myData.distance_2_mm && phase_one_for_out && !phase_one_for_in)
-  {
-    Serial.println("out");
-    phase_one_for_out = false;
-    phase_one_for_in = false;
-    phase_one_for_out = false;
-    myData.direction = 0;
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-    t1 = 0;
-    t2 = 0;
   }
 
+  if (VL53LOX_2_State == LOW)
+  {
+    Serial.println("Sensor 2 interupt");
+    lox2.clearInterruptMask(false);
+
+  }
   // if((saved_distance_1 - myData.distance_1_mm) > 1000 && (saved_distance_2 - myData.distance_2_mm) < 500){
   //   if (phase_one_for_out == true && t1 < t2){
   //     direction_in = false;
@@ -318,6 +352,26 @@ void setID()
     while (1)
       ;
   }
+  Serial.println("Set GPIO Config so if range is lower the LowThreshold "
+                 "trigger Gpio Pin ");
+  lox1.setGpioConfig(VL53L0X_DEVICEMODE_CONTINUOUS_RANGING,
+                     VL53L0X_GPIOFUNCTIONALITY_THRESHOLD_CROSSED_LOW,
+                     VL53L0X_INTERRUPTPOLARITY_LOW);
+
+  // Set Interrupt Treashholds
+  // Low reading set to 50mm  High Set to 100mm
+  FixPoint1616_t LowThreashHold_1 = (50 * 65536.0);
+  FixPoint1616_t HighThreashHold_1 = (100 * 65536.0);
+  Serial.println("Set Interrupt Threasholds... ");
+  lox1.setInterruptThresholds(LowThreashHold_1, HighThreashHold_1, true);
+
+  // Enable Continous Measurement Mode
+  Serial.println("Set Mode VL53L0X_DEVICEMODE_CONTINUOUS_RANGING... ");
+  lox1.setDeviceMode(VL53L0X_DEVICEMODE_CONTINUOUS_RANGING, false);
+
+  Serial.println("StartMeasurement... ");
+  lox1.startMeasurement();
+
   delay(10);
 
   // activating LOX2
@@ -331,4 +385,23 @@ void setID()
     while (1)
       ;
   }
+  Serial.println("Set GPIO Config so if range is lower the LowThreshold "
+                 "trigger Gpio Pin ");
+  lox2.setGpioConfig(VL53L0X_DEVICEMODE_CONTINUOUS_RANGING,
+                     VL53L0X_GPIOFUNCTIONALITY_THRESHOLD_CROSSED_LOW,
+                     VL53L0X_INTERRUPTPOLARITY_LOW);
+
+  // Set Interrupt Treashholds
+  // Low reading set to 50mm  High Set to 100mm
+  FixPoint1616_t LowThreashHold = (50 * 65536.0);
+  FixPoint1616_t HighThreashHold = (100 * 65536.0);
+  Serial.println("Set Interrupt Threasholds... ");
+  lox2.setInterruptThresholds(LowThreashHold, HighThreashHold, true);
+
+  // Enable Continous Measurement Mode
+  Serial.println("Set Mode VL53L0X_DEVICEMODE_CONTINUOUS_RANGING... ");
+  lox2.setDeviceMode(VL53L0X_DEVICEMODE_CONTINUOUS_RANGING, false);
+
+  Serial.println("StartMeasurement... ");
+  lox2.startMeasurement();
 }
